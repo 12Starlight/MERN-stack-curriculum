0cf3e5fa8b2ed20ebc14825e6d6a7d3a7c02c11a
Dave <dave.gagnat@gmail.com>
2020-04-22 18:56:15 -0500

    Added mongoDB

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://12Starlight:i6k2IhpakQ9r9tGW@cluster0-z0cko.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://12Starlight:P8Flgn48cYCoPFIS@cluster0-z0cko.mongodb.net/test?retryWrites=true&w=majority'
 }
