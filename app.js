4eb79dcc20ddc9e00738f6ca67b0ad19162d39b3
Dave <dave.gagnat@gmail.com>
2020-04-23 03:31:26 -0500

    Added debugging script for nodemon

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI; 
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const bodyParser = require('body-parser');
 
 
-app.get('/', (req, res) => res.send('Hello World!')); 
+app.get('/', (req, res) => {
+  console.log(res);
+  res.send('Hello World!');
+}); 
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
