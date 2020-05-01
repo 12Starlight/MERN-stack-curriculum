1f2f9aa98dee9edc3714a1964460be9ca445d307
Dave <dave.gagnat@gmail.com>
2020-05-01 16:37:11 -0500

    Updated passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,13 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const keys = require('../config/keys');
+const User = mongoose.model('users');
+const keys = require('./keys');
 
 
 const options = {};
-options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = keys.secretOrKey;
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); // extracts the bear jsonweb token from the header
+options.secretOrKey = keys.secretOrKey; 
 
 module.exports = passport => {
   passport.use(new JwtStrategy(options, (jwt_payload, done) => {
