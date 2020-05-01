3031f302bf30155071327e406e8f9b0bb8acd10a
Dave <dave.gagnat@gmail.com>
2020-05-01 16:17:50 -0500

    Modified app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
-require('./config/passport')(passport);
+// require('./config/passport')(passport);
 
 const db = require('./config/keys').mongoURI; 
 const users = require('./routes/api/users');
@@ -15,6 +15,9 @@ mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(error => console.log(error));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   // debugger; 
@@ -32,8 +35,7 @@ app.get('/', (req, res) => {
 // Express Router 
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
+
 app.use(passport.initialize());
 
 const port = process.env.PORT || 5000; 
