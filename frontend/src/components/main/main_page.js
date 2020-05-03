efde61fea32e9eca9ea84c406e746c2963d36ce4
Dave <dave.gagnat@gmail.com>
2020-05-03 15:25:30 -0500

    Created MainPage component

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class MainPage extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>A Twitter Clone</h1>
+        <footer>
+          Copyright &copy; 2020 Starlight
+        </footer>
+      </div>
+    );
+  }
+}
+
+
+export default MainPage; 
