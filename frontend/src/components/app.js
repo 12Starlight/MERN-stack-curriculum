e6ab1d33c0613e26b11a2344adcc5986fabb5114
Dave <dave.gagnat@gmail.com>
2020-05-03 15:32:24 -0500

    Created additional auth routes for app

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,14 +1,26 @@
+// Import React Redux
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 
+// Import Local Directory Files
+import NavBarContainer from './nav/navbar_container';
+
+// Auth Routes
 import MainPage from './main/main_page';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
 
 
 const App = () => (
-  <Switch>
-    <AuthRoute exact path='/' component={MainPage} />
-  </Switch>
+  <div>
+    <NavBarContainer />
+    <Switch>
+      <AuthRoute exact path='/' component={MainPage} />
+      <AuthRoute exact path='/login' component={LoginFormContainer} />
+      <AuthRoute exact path='/signup' component={SignupFormContainer} />
+    </Switch>  
+  </div>
 );
 
 
