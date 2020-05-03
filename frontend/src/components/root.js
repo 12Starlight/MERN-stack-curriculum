474d2fd1602014c8bd253f72d1ff22297f17992d
Dave <dave.gagnat@gmail.com>
2020-05-03 14:58:57 -0500

    Built root.js

---
+++ frontend/src/components/root.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+// We will create this soon
+import App from './app'; 
+
+
+const Root = ({ store }) => (
+  <Provider store={ store } >
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
+);
+
+
+export default Root; 
