d62a11881f141840c72028be43bc196bdaafc5f5
Dave <dave.gagnat@gmail.com>
2020-05-05 14:23:57 -0500

    Built key value pairs, connected to heroku, created script

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
 // require('./config/passport')(passport);
+const path = require('path');
 
 const db = require('./config/keys').mongoURI; 
 const users = require('./routes/api/users');
@@ -42,6 +43,12 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 
 
